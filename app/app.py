9821498176455b61eeaca926b46b1e633128f05e
Leonardo Bilhalva <leobbbilhalva@gmail.com>
2023-09-12 18:23:54 -0300

    fix(app): fixed table names and added two new functions

--- app/app.py
+++ app/app.py
@@ -7,6 +7,28 @@ db_pass = 'secret'
 db_host = 'localhost'
 db_port = '5432'
 
+
+def queryGeneral(cursor):
+
+    cursor.execute(
+        "SELECT * FROM processo_seletivos_graduacao WHERE curso like 'Admin%'")
+    rows = cursor.fetchall()
+
+
+def queryCourseEvasionOne(cursor):
+
+    cursor.execute(
+        "SELECT nome_curso, SUM(evadidos) as total_evadidos FROM quantitativo_alunos WHERE ano >= 2023 - 5 GROUP BY nome_curso ORDER BY total_evadidos DESC LIMIT 5;")
+    rows = cursor.fetchall()
+
+
+def queryCourseEvasionTwo(cursor):
+
+    cursor.execute(
+        "SELECT nome_curso, SUM(evadidos) as total_evadidos FROM quantitativo_alunos WHERE ano >= 2023 - 13 GROUP BY nome_curso ORDER BY total_evadidos DESC LIMIT 5;")
+    rows = cursor.fetchall()
+
+
 if __name__ == '__main__':
     print('Application started')
     conn = None  # Initialize conn and cursor variables
@@ -20,16 +42,18 @@ if __name__ == '__main__':
 
         with open('files/dadosabertos_graduacao_processos_seletivos.csv', 'r') as file:
             cursor.copy_expert(
-                sql="COPY ProcessoSeletivosGraduacao FROM stdin WITH CSV HEADER DELIMITER ';'", file=file)
+                sql="COPY Processo_Seletivos_Graduacao FROM stdin WITH CSV HEADER DELIMITER ';'", file=file)
         conn.commit()  # add data from csv to ProcessoSeletivosGraduacao and commit
 
         with open('files/dadosabertos_graduacao_quantitativo-de-alunos.csv', 'r') as file:
             cursor.copy_expert(
-                sql="COPY QuantitativoAlunosGraduacao FROM stdin WITH CSV HEADER DELIMITER ';'", file=file)
+                sql="COPY Quantitativo_Alunos_Graduacao FROM stdin WITH CSV HEADER DELIMITER ';'", file=file)
         conn.commit()  # add data from csv to QuantitativoAlunosGraduacao and commit
 
         while True:  # execute queries here
-            print('The last value inserted is: zessro')
+            print('The last value inserted is: zero')
+            queryGeneral(cursor)
+
             time.sleep(5)
 
     except Exception as e:
